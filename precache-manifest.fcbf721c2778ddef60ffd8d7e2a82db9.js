3eb6cb6f60f2d934006c90df340f910492c94b7d
Roman Kucinski <roman.kucinski@gmail.com>
2018-11-16 13:47:49 +0100

    Updates

---
+++ precache-manifest.fcbf721c2778ddef60ffd8d7e2a82db9.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = [
+  {
+    "revision": "2b8f8d626a0486d814e87649e8776d1f",
+    "url": "/Potter_Clash/static/media/coupe3.2b8f8d62.png"
+  },
+  {
+    "revision": "7b5e047a2986fb3e9d4a",
+    "url": "/Potter_Clash/static/css/main.d47c3239.chunk.css"
+  },
+  {
+    "revision": "c904dc21fbaa06facc4fa6bdad19c72c",
+    "url": "/Potter_Clash/static/media/sybilltrelawney.c904dc21.jpeg"
+  },
+  {
+    "revision": "393d92864b51c3f56634",
+    "url": "/Potter_Clash/static/js/1.393d9286.chunk.js"
+  },
+  {
+    "revision": "de571e3613868cf9ba4e",
+    "url": "/Potter_Clash/static/js/runtime~main.de571e36.js"
+  },
+  {
+    "revision": "23167bc6d343b9b95c483ab65d1e01bc",
+    "url": "/Potter_Clash/static/media/poudlard.23167bc6.jpg"
+  },
+  {
+    "revision": "46362f38f4a6d32b543a5ebaca26eda8",
+    "url": "/Potter_Clash/static/media/logoHPC.46362f38.png"
+  },
+  {
+    "revision": "14ac166b89be136b3f40839bd1bf914d",
+    "url": "/Potter_Clash/static/media/keyLeft.14ac166b.png"
+  },
+  {
+    "revision": "aca2b5ffe125cc934adfd9c384eefcc1",
+    "url": "/Potter_Clash/static/media/keyRight.aca2b5ff.png"
+  },
+  {
+    "revision": "7b5e047a2986fb3e9d4a",
+    "url": "/Potter_Clash/static/js/main.7b5e047a.chunk.js"
+  },
+  {
+    "revision": "43b1a27afa42e4b94d23d53902ffda4c",
+    "url": "/Potter_Clash/static/media/harrypotter.43b1a27a.jpeg"
+  },
+  {
+    "revision": "fab96e7856609f59f368fa8c5a1b29b9",
+    "url": "/Potter_Clash/static/media/cedricdiggory.fab96e78.jpeg"
+  },
+  {
+    "revision": "3414ee69b0fcb86c521e28588c007d8f",
+    "url": "/Potter_Clash/static/media/dragomalfoy.3414ee69.jpeg"
+  },
+  {
+    "revision": "c98fb44159cf6d98663cad91b246c53b",
+    "url": "/Potter_Clash/static/media/lunalovegood.c98fb441.jpeg"
+  },
+  {
+    "revision": "876784d98d314c68ac4cdebe7e57de8e",
+    "url": "/Potter_Clash/static/media/hermionegranger.876784d9.jpeg"
+  },
+  {
+    "revision": "c392bb5eb0b8777fd1bded963eff3faa",
+    "url": "/Potter_Clash/static/media/norbertdragonneau.c392bb5e.jpeg"
+  },
+  {
+    "revision": "c9328628bec7499f9c540e7f06ef3e2a",
+    "url": "/Potter_Clash/static/media/severussnape.c9328628.jpeg"
+  },
+  {
+    "revision": "393d92864b51c3f56634",
+    "url": "/Potter_Clash/static/css/1.a6605e34.chunk.css"
+  },
+  {
+    "revision": "265f43c8cd78010a4986eeff05b40742",
+    "url": "/Potter_Clash/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.2/workbox-sw.js");
+
+importScripts(
+  "/Potter_Clash/precache-manifest.fcbf721c2778ddef60ffd8d7e2a82db9.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/Potter_Clash/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
